52d044b0ba77a16f524b1d15c5e1aaaaa9708a24
moroball14 <daiki.mr.0330@gmail.com>
2025-02-02 12:34:07 +0900

    fix: insert without blocking by rate limit

--- src/googleCalenderEventCreator.ts
+++ src/googleCalenderEventCreator.ts
@@ -7,13 +7,15 @@ export class GoogleCalendarEventCreator {
     this.client = new calendar_v3.Calendar({ auth });
   }
   public async bulkInsert(events: GoogleCalendarEvent[]): Promise<void> {
-    // 1秒に5回までしかリクエストを送れないので、5並列でリクエストを送って1.5秒待つ
-    const promises = events.map((event) =>
-      this.client.events.insert({ calendarId: "primary", requestBody: event })
-    );
-    for (let i = 0; i < promises.length; i += 5) {
-      await Promise.all(promises.slice(i, i + 5));
-      await new Promise((resolve) => setTimeout(resolve, 1500));
+    for (const event of events) {
+      // rate limit対策
+      // 5 並列でリクエスト & 2秒sleep でも引っかかったので、 0.5 秒ずつ 1 リクエスト
+      console.log("イベントを追加します", event);
+      await this.client.events.insert({
+        calendarId: "primary",
+        requestBody: event,
+      });
+      await new Promise((resolve) => setTimeout(resolve, 500));
     }
   }
 }
